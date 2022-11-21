90b805198162ad29d78ed720c9b151428d96c8e1
Reza Sadeghi <83159480+staralgorithem@users.noreply.github.com>
2022-11-21 11:55:50 +0330

    Update simple_Reg.py

--- simple_Reg.py
+++ simple_Reg.py
@@ -6,3 +6,8 @@ from sklearn.linear_model import LinearRegression
 
 x = np.random.rand(100, 1)
 y = 4 + 5 * x + np.random.rand(100, 1)
+
+reg = LinearRegression()
+reg.fit(x, y)
+
+x_vals = np.linspace(0, 1, 100).reshape(-1, 1)
